6340a59684c3daebd59a386f66aecace38bf6af6
Mohammad-Ehshan <ashmes16@gmail.com>
2025-09-01 17:13:32 +0530

    fixing deployment error

--- src/components/magicui/bento-grid.tsx
+++ src/components/magicui/bento-grid.tsx
@@ -1,8 +1,8 @@
-import { ArrowRightIcon } from "@radix-ui/react-icons";
 import { ComponentPropsWithoutRef, ReactNode } from "react";
 
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/index";
+import { ArrowRightIcon } from "lucide-react";
 
 interface BentoGridProps extends ComponentPropsWithoutRef<"div"> {
   children: ReactNode;
--- src/components/marketing/integration.tsx
+++ src/components/marketing/integration.tsx
@@ -126,3 +126,6 @@ const Integration = () => {
 };
 
 export default Integration;
+
+
+
